c5f73f7da6f5aab968d149d192fd03e8375e9c63
ahmadjon2008 <robotoprotecno2008@gmail.com>
2024-04-19 17:33:33 +0500

    Commit and push

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+import uvicorn
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+colors = [{"name": "red", "type": "HTML"}, {"name": "Blue", "type": "Python"}]
+
+
+@app.get("/")
+async def root(request: Request):
+    return templates.TemplateResponse("main.html", {"request": request, "user": "All_Linux", "colors": colors})
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000, )
